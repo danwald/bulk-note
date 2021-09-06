16c771c7ecdac88dc594cff6fc011e50ec3c6dc1
danny crasto <danwald79@gmail.com>
2021-09-06 23:54:38 +0400

    making single

--- bulk_note/imi_message.py
+++ bulk_note/imi_message.py
@@ -1,38 +1,49 @@
 import pickle
 import xml.etree.ElementTree as ET
 from dataclasses import dataclass
-from typing import Optional, io
+from typing import Optional, List, Dict, io
+
+from io import StringIO
 
 from . import message
 
 
+FROM = "+121212"
+CONTENT = "foo bar"
+CID_PREFIX = "2021-09-07"
+
+
 @dataclass
 class IMIPayload(message.Payload):
-    cid: str
-    to: str
-    frm: str
-    content: str
-    send_on_group: Optional[str] = None
+    cid = 0
+    numbers: List[str]
+    send_codes: Optional[Dict[str, str]] = None
 
     def dumps(self) -> str:
-        payload = (
-            f'<submitRequest id="{self.cid}">'
-            "<from>{self.frm}</from>"
-            "<to>{self.to}</to>"
-            '<content type="text">{self.content}</content>'
-        )
-        if self.send_on_group:
-            payload = f'{payload}<sendOnGroup value="{self.send_on_group}"/>'
-        return f"{payload}</submitRequest>"
+        payload = StringIO()
+
+        for to in self.numbers:
+            payload.write(
+                (
+                    f'<submitRequest id="{CID_PREFIX}_{IMIPayload.cid:07}">'
+                    "<from>{FROM}</from>"
+                    "<to>{to}</to>"
+                    '<content type="text">{CONTENT}</content>'
+                )
+            )
+            IMIPayload.cid += 1
+            send_on_group = self.send_codes and self.send_codes.get(to)
+            if send_on_group:
+                payload.write(f"{payload}<sendOnGroup" 'value="{self.send_on_group}"/>')
+        return f"<xiamSMS>{payload.getvalue()}</submitRequest><xiamSMS>"
 
 
-class IMIRecipient(message.Recipient):
+class IMIRecipients:
     def __init__(
-        self, receipients: io.TextIO, send_groups: io.BinaryIO, content: str
+        self, receipients: io.TextIO, send_groups: Optional[io.BinaryIO]
     ) -> None:
         self.receipients = filter(None, (line.trim() for line in receipients))
-        self.send_groups = pickle.load(send_groups)
-        self.content = content
+        self.send_groups = pickle.load(send_groups) if send_groups else None
 
     def get_tx_payload(self, count: int = 1) -> IMIPayload:
         pass
--- tests/test_imi_message.py
+++ tests/test_imi_message.py
@@ -3,27 +3,10 @@ import pytest
 from bulk_note import imi_message
 
 
-PAYLOAD_PARTIAL = {
-    "cid": "1",
-    "to": "+971529492034",
-    "frm": "+97150923823",
-    "content": "foo bar mofo",
-}
-
-PAYLOAD_FULL = {
-    "cid": "2",
-    "to": "+971529492034",
-    "frm": "+97150923823",
-    "content": "foo bar mofo",
-    "send_on_group": "123321",
-}
+PAYLOAD = {"numbers": ["+971529492034", "+97150923823"]}
 
 
 def test_impayload_full_dumps():
-    payload = imi_message.IMIPayload(**PAYLOAD_FULL)
-    assert payload.dumps().find("sendOnGroup") != -1
-
-
-def test_impayload_partial_duumps():
-    payload = imi_message.IMIPayload(**PAYLOAD_PARTIAL)
-    assert payload.dumps().find("sendOnGroup") == -1
+    payload = imi_message.IMIPayload(**PAYLOAD)
+    print(f"{payload.dumps()}")
+    assert payload.dumps()
