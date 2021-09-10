6f60ef353ed20e768342633cf6d3fbc4c2ab7722
danny crasto <danwald79@gmail.com>
2021-09-10 11:06:34 +0400

    compounded with

--- bulk_note/cli.py
+++ bulk_note/cli.py
@@ -30,24 +30,21 @@ logger = logging.getLogger(__name__)
 @click.option("-v", "--verbose", count=True)
 def main(numbers, content, send_codes, bulk_size, verbose):
     receipients = IMIRecipients(numbers, content, send_codes, bulk_size, verbose)
-    with open("./good.out", "a") as good_out:
-        with open("./fail.out", "a") as fail_out:
-            while payload := receipients.get_tx_payload().dumps():  # noqa:  E231
-                time.sleep(1)
-                try:
-                    resp = requests.post(SERVER_URL, data=payload, headers=HEADERS)
-                    resp.raise_for_status()
-                    logger.debug("Sent bulk")
-                    for ir in IMIResponse("OK", resp.text).process().get_get_success():
-                        good_out.write(f"{ir}\n")
-                except requests.HTTPError as e:  # noqa F821
-                    logger.exception("Except when sending Failed to send request")
-                    fail_out.write(
-                        f"Bad Response {resp.status_code}:\n{resp.text}\n<>\n"
-                    )
-                except xml.etree.ElementTree.ParseError as p:  # noqa F821
-                    logger.exception("XmlParsing error")
-                    fail_out.write(f"XmlParser:\n{resp.text}\n<>\n")
+    with open("./good.out", "a") as good_out, open("./fail.out", "a") as fail_out:
+        while payload := receipients.get_tx_payload().dumps():  # noqa:  E231
+            time.sleep(1)
+            try:
+                resp = requests.post(SERVER_URL, data=payload, headers=HEADERS)
+                resp.raise_for_status()
+                logger.debug("Sent bulk")
+                for ir in IMIResponse("OK", resp.text).process().get_get_success():
+                    good_out.write(f"{ir}\n")
+            except requests.HTTPError as e:  # noqa F821
+                logger.exception("Except when sending Failed to send request")
+                fail_out.write(f"Bad Response {resp.status_code}:\n{resp.text}\n<>\n")
+            except xml.etree.ElementTree.ParseError as p:  # noqa F821
+                logger.exception("XmlParsing error")
+                fail_out.write(f"XmlParser:\n{resp.text}\n<>\n")
 
     return 0
 
